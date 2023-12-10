8a806fc460115e4c43971fbbdb3b7cb47a24d306
ashbirdhiman <38061870+ashbirdhiman@users.noreply.github.com>
2023-12-10 12:33:32 -0500

    adding project fiiles
    Includes design using MVC architecture
    Includes PHP files
    Includes project requirements

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!--Head-->
+@include('includes.head', ['title' => ''])
+
+<!-- Topbar -->
+@include('includes.Topbar')
+
+<!-- Novbar -->
+@include('includes.navbar_index')
+
+<!-- feature -->
+@include('includes.feature')
+
+<!-- Category start-->
+@include('includes.category')
+<!-- Category End-->
+
+
+<!-- Subscribe Start -->
+@include('includes.stay_updated')
+<!-- Subscribe End -->
+
+
+
+<!-- Include file from include/footer.blade.php file -->
+@include('includes.footer')
